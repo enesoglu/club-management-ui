97426d8a9a04f1f6767b839256ac7cd05761d010
enes yıldız <enesyildizonline@hotmail.com>
2025-08-07 08:58:38 +0300

    chore

--- src/app/features/member-list/member-list.component.ts
+++ src/app/features/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { MemberService } from '../../core/services/member.service';
 import { ClubMember, MemberRole, MembershipStatus } from '../../core/models/club-member.model';
 
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -14,10 +14,10 @@ import { IconField } from 'primeng/iconfield';
 import { InputIcon } from 'primeng/inputicon';
 import { InputText } from 'primeng/inputtext';
 import { MultiSelect } from 'primeng/multiselect';
-import {Menu} from 'primeng/menu';
-import {MenuItem, MessageService} from 'primeng/api';
-import {FileUpload} from 'primeng/fileupload';
-import {Toast} from 'primeng/toast';
+import { Menu } from 'primeng/menu';
+import { MenuItem, MessageService } from 'primeng/api';
+import { FileUpload } from 'primeng/fileupload';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-member-list',
@@ -79,19 +79,6 @@ export class MemberListComponent implements OnInit {
     ];
   }
 
-  // response "200 OK" from backend
-  onCsvUpload(event: any) {
-    const response = JSON.parse(event.xhr.response); // get response from backend
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: response.body || 'Members imported successfully.' });
-    this.loadMembers();
-  }
-
-  // error while uploading
-  onCsvError(event: any) {
-    const errorResponse = JSON.parse(event.xhr.response); // get response from backend
-    this.messageService.add({ severity: 'error', summary: 'Error', detail: errorResponse.body || 'File could not be imported.' });
-  }
-  
   loadMembers(): void {
     this.memberService.getMembers().subscribe({
       next: (data: ClubMember[]) => {
@@ -199,5 +186,19 @@ export class MemberListComponent implements OnInit {
 
   isFirstPage(): boolean {
     return this.filteredMembers ? this.first === 0 : true;
+  }
+
+  // import via csv upload feature
+  // response "200 OK" from backend
+  onCsvUpload(event: any) {
+    const response = JSON.parse(event.xhr.response); // get response from backend
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: response.body || 'Members imported successfully.' });
+    this.loadMembers();
+  }
+
+// error while uploading
+  onCsvError(event: any) {
+    const errorResponse = JSON.parse(event.xhr.response); // get response from backend
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: errorResponse.body || 'File could not be imported.' });
   }
 }
