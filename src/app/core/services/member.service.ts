6e22283b301724888dd21c6457b86adfed37d230
Enes Yıldız <yildiz_enes@hotmail.com>
2025-08-29 14:34:27 +0300

    Add methods to retrieve member's active position
    Introduces findActivePosition, getActivePosition, and getActivePositionString methods to MemberService for accessing and formatting a member's active position and related team information.

--- src/app/core/services/member.service.ts
+++ src/app/core/services/member.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { ClubMember } from '../models/club-member.model';
+import {Position, Team} from '../models/position.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,37 @@ export class MemberService {
     return this.http.post<ClubMember>(this.apiUrl, member)
   }
 
+  findActivePosition(member: ClubMember): Position | undefined {
+    if (!member.positions || member.positions.length === 0) {
+      return undefined;
+    }
+    return member.positions.find(p => p.isActive);
+  }
+
+  getActivePosition(member: ClubMember): Team | undefined {
+    const activePosition = this.findActivePosition(member);
+
+    return activePosition?.team;
+  }
+
+  // get member's active position
+  getActivePositionString(member: ClubMember): string {
+    const activePosition = this.findActivePosition(member);
+
+    if (!activePosition) {
+      return "no active position";
+    }
+    switch (activePosition.team) {
+
+      case Team.EXECUTIVE:
+        return `${activePosition.team} (${activePosition.executiveTitle})`;
+
+      case Team.CREW:
+        return `${activePosition.team} (${activePosition.crewComittee})`;
+
+      default:
+        return activePosition.team;
+    }
+  }
 }
 
