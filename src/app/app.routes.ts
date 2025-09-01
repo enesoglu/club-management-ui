a020e4fa748d1fd63c11f6a59e236971b3f82a9e
Enes Yıldız <yildiz_enes@hotmail.com>
2025-09-01 16:22:54 +0300

    Add MemberDetailComponent and route
    Introduces MemberDetailComponent with its template and style files. Updates app routes to include a protected 'members/:id' path for viewing member details.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './features/login/login.component';
 
 import { authGuard } from './core/guards/auth-guard';
 import { loginGuard } from './core/guards/login-guard';
+import { MemberDetailComponent } from './features/member-detail/member-detail.component';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,12 @@ export const routes: Routes = [
   {
     path: 'members',
     component: MemberListComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'members/:id',
+    component: MemberDetailComponent,
     canActivate: [authGuard]
   },
 
---
+++ src/app/features/member-detail/member-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+@Component({
+  selector: 'app-member-detail',
+  imports: [
+    Header
+  ],
+  templateUrl: './member-detail.component.html',
+  styleUrl: './member-detail.component.css'
+})
+export class MemberDetailComponent {
+
+}
