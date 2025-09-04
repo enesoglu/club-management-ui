8c15ba66a42df03d009a75fdfda44cd1f283fa39
Enes Yıldız <yildiz_enes@hotmail.com>
2025-09-04 11:38:37 +0300

    Add Term model and service

---
+++ src/app/core/services/term.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Term} from '../models/term.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PositionService {
+
+  private apiUrl = 'http://localhost:8080/api/terms';
+
+  constructor(private http: HttpClient) {}
+
+  getTerms(): Observable<Term[]> {
+    return this.http.get<Term[]>(this.apiUrl);
+  }
+
+}
