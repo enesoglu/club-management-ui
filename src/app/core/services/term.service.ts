26fad8378eb8ad78121fb3050d62db42f03ac931
Enes Yıldız <yildiz_enes@hotmail.com>
2025-09-04 18:14:14 +0300

    Implement CRUD operations for TermService

--- src/app/core/services/term.service.ts
+++ src/app/core/services/term.service.ts
@@ -16,4 +16,25 @@ export class TermService {
     return this.http.get<Term[]>(this.apiUrl);
   }
 
+  getActiveTerm(): Observable<Term> {
+    return this.http.get<Term>((`${this.apiUrl}/get-active-term`));
+  }
+
+  setActiveTerm(term: Term): Observable<Term> {
+    return this.http.put<Term>((`${this.apiUrl}/set-active/${term.id}`), null);
+  }
+
+  deleteTerm(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete-term/${id}`);
+  }
+
+  saveTerm(term: Term): Observable<Term> {
+    if (term.id)
+      // if term has an id, it's an update
+      return this.http.put<Term>(`${this.apiUrl}/update-term/${term.id}`, term);
+    else
+      // if not, it's a new term
+    return this.http.post<Term>(`${this.apiUrl}/create-term`, term);
+  }
+
 }
