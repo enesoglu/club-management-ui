d6322ea0a6dc6b7743ec48c7b857d092846aefaf
Enes Yıldız <yildiz_enes@hotmail.com>
2025-09-04 18:11:33 +0300

    Fix term service name

--- src/app/core/services/term.service.ts
+++ src/app/core/services/term.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class PositionService {
+export class TermService {
 
   private apiUrl = 'http://localhost:8080/api/terms';
 
